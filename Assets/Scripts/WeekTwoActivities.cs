40ef5250e94d360ca35e2e04d9b51842afcd7abe
tomasdennettsae <107592319+tomasdennettsae@users.noreply.github.com>
2022-06-16 11:05:38 +1000

    Initial commit

---
+++ Assets/Scripts/WeekTwoActivities.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TomasDennett
+{
+
+    public class WeekTwoActivities : MonoBehaviour
+    {
+        private string favouriteGame = "Grand Theft Auto";
+        private int hoursPlayed = 30;
+        private float gamePrice = 89.95f;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+
+}
