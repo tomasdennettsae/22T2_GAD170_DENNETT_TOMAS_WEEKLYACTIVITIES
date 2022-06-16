80c6ec4dbab305967712f365e90c93ca925fcd51
tomasdennettsae <107592319+tomasdennettsae@users.noreply.github.com>
2022-06-16 11:44:50 +1000

    Output my name to console

---
+++ Assets/Scripts/WeekThreeActivities.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeekThreeActivities : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Speech();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Speech()
+    {
+        Debug.Log("My name is Tomas");
+    }
+}
